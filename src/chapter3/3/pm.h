7a35c13a1ab5cfa880f9744a1277d424e9c7605e
solrex <solrex@e00b6d85-c64c-0410-ad21-8bb8131ca1a4>
2008-09-13 08:24:53 +0000

    Updated demo code with micro InitDesc.

--- src/chapter3/3/pm.h
+++ src/chapter3/3/pm.h
@@ -80,3 +80,17 @@
     .2byte     (\PCount & 0x1F) | ((\Attr << 8) & 0xFF00)
     .2byte     ((\Offset >> 16) & 0xFFFF)
 .endm
+
+/* Initialize descriptor.
+   Usage: InitDesc SegLabel, SegDesc */
+.macro InitDesc SegLabel, SegDesc
+    xor     %eax, %eax
+    mov     %cs, %ax
+    shl     $4, %eax
+    addl    $(\SegLabel), %eax
+    movw    %ax, (\SegDesc + 2)
+    shr     $16, %eax
+    movb    %al, (\SegDesc + 4)
+    movb    %ah, (\SegDesc + 7)
+.endm
+
